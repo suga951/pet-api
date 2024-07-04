2af6ebc22761023f721f4db690790dd6af9e9935
Agustin Lopez <aalopezba@gmail.com>
2024-07-04 15:42:14 -0300

    added http status

---
+++ controllers/userController.js
@@ -0,0 +1,6 @@
+import { PrismaClient } from "@prisma/client";
+import httpStatus from "./helpers/httpStatus.js";
+
+const prisma = new PrismaClient();
+
+export const userController = () => {};
--- index.js
+++ index.js
@@ -9,7 +9,27 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (_req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! //Login page");
+});
+
+app.get("/register", (_req, res) => {
+  res.send("Register here.");
+});
+
+app.get("/profile", (_req, res) => {
+  res.send("Profile here.");
+});
+
+app.get("/api/pets", (__req, res) => {
+  res.send("Pets endpoint here.");
+});
+
+app.get("/api/vet", (_req, res) => {
+  res.send("Vet endpoint here.");
+});
+
+app.get("/api/client", (_req, res) => {
+  res.send("Client endpoint here.");
 });
 
 app.listen(PORT, () => {
