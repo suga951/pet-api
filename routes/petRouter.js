2aba7f6eba5dbf3ed344c4af433e9952a33a81de
Agustin Lopez <aalopezba@gmail.com>
2024-07-26 06:54:43 -0300

    added delete by id routes

--- routes/petRouter.js
+++ routes/petRouter.js
@@ -3,10 +3,10 @@ import { petController } from "../controllers/petController.js";
 
 export const petRoutes = () => {
   const petRouter = Router();
-  const { getPets, addPet, getPetById } = petController();
+  const { getPets, addPet, getPetById, deletePetById } = petController();
 
   petRouter.route("/pets").get(getPets).post(addPet);
 
-  petRouter.route("/pets/:id").get(getPetById);
+  petRouter.route("/pets/:id").get(getPetById).delete(deletePetById);
   return petRouter;
 };
