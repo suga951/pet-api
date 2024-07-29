1ed412265c0dd77aa44f6eac7eb81fe9213a66a9
Agustin Lopez <aalopezba@gmail.com>
2024-07-29 11:31:09 -0300

    feat: Add errorHandler

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import userRouter from "./routes/userRouter.js";
 import { petRoutes } from "./routes/petRouter.js";
 
 dotenv.config();
 
 const PORT = process.env.SERVER_PORT || 3002;
-const app = express();
 
+const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  }),
+);
 
 app.use("/api", petRoutes(), userRouter());
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
