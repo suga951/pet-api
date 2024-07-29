97fc33846f344629d161bf42de56b00a9e593cb6
Agustin Lopez <aalopezba@gmail.com>
2024-07-29 11:08:56 -0300

    feat: Add Token logic

---
+++ utils/tokenManagement.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = ({
+  data,
+  expiresIn = "10s",
+  isRefresh = false,
+}) => {
+  const secretKey = isRefresh
+    ? process.env.REFRESH_SECRET_KEY
+    : process.env.SECRET_KEY;
+  const token = jwt.sign(data, secretKey, {
+    expiresIn,
+  });
+  return token;
+};
+
+export const verifyToken = (token) => {
+  const secretKey = process.env.REFRESH_SECRET_KEY;
+  return jwt.verify(token, secretKey);
+};
