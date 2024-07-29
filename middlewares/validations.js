09985b5fc36c091f8f006d304d7e0629e95d8564
Agustin Lopez <aalopezba@gmail.com>
2024-07-29 11:07:54 -0300

    feat: Add validations

---
+++ middlewares/validations.js
@@ -0,0 +1,16 @@
+import { query } from "express";
+
+export const schemaValidator = (schema) => async (req, res, next) => {
+  const { error } = schema.validate(
+    {
+      body: req.body,
+      params: req.params,
+      query: req.query,
+    },
+    {
+      abortEarly: false,
+      allowUnknown: true,
+    },
+  );
+  error ? next(error) : next();
+};
--- routes/petRouter.js
+++ routes/petRouter.js
@@ -1,17 +1,22 @@
 import { Router } from "express";
 import { petController } from "../controllers/petController.js";
+import { schemaValidator } from "../middlewares/validations.js";
+import { petSchema, petIdSchema } from "../schemas/petSchema.js";
 
 export const petRoutes = () => {
   const petRouter = Router();
   const { getPets, addPet, getPetById, deletePetById, updatePetById } =
     petController();
 
-  petRouter.route("/pets").get(getPets).post(addPet);
+  petRouter
+    .route("/pets")
+    .get(getPets)
+    .post(schemaValidator(petSchema), addPet);
 
   petRouter
     .route("/pets/:id")
     .get(getPetById)
     .delete(deletePetById)
-    .patch(updatePetById);
+    .patch(schemaValidator(petIdSchema), updatePetById);
   return petRouter;
 };
